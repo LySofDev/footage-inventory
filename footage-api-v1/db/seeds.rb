c9be53bc72891e6ebf45861d0c35e15ca2915ce1
Esteban Hernandez <hernandez.dev.services@gmail.com>
2018-09-23 15:02:12 -0400

    FI-003 Added basic Database seed

--- footage-api-v1/db/seeds.rb
+++ footage-api-v1/db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+FactoryBot.find_definitions
+
+25.times do
+  video = FactoryBot.create( :video )
+  ( rand( 4 ) + 1 ).times do
+    FactoryBot.create( :clip, video: video )
+  end
+end
