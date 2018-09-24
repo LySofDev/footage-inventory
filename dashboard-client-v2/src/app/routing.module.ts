a1e1e2eeeac42f82d9755b4b4a648949be225f5b
Esteban Hernandez <hernandez.dev.services@gmail.com>
2018-09-23 21:02:33 -0400

    FI-007 Added Dashboard page

--- dashboard-client-v2/src/app/routing.module.ts
+++ dashboard-client-v2/src/app/routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { DashboardComponent } from "./dashboard/dashboard.component";
 import { VideosComponent } from './videos/videos.component';
 import { ClipsComponent } from './clips/clips.component';
 
 const routes: Routes = [
+  { path: '', component: DashboardComponent },
   { path: 'videos', component: VideosComponent, children: [
     { path: ':id/clips', component: ClipsComponent }
   ] },
